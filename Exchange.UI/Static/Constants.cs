3927bbe1a1647b29c37dd9689183c7266db6c956
Jarlyn Polanco <jarlynpolanco@gmail.com>
2021-05-04 14:32:08 -0400

    Casting decimal fields to amount and adding the currency constant

--- Exchange.UI/Static/Constants.cs
+++ Exchange.UI/Static/Constants.cs
@@ -1,10 +1,11 @@
 ﻿namespace Exchange.UI.Static
 {
-    public static class Endpoints
+    public static class Constants
     {
         public static string BaseUrl = "https://localhost:5001/";
         public static string ExchangeRateEndpoint = $"{BaseUrl}api/ExchangeRate/Rate/";
         public static string ExchangePurchaseEndpoint = $"{BaseUrl}api/ExchangeRate/Purchase/";
         public static string AllUsersEndpoint = $"{BaseUrl}api/User/AllUsers/";
+        public static string[] Currencies = { "USD", "BRL" };
     }
 }
